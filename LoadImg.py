8f85091026c85ff7c9e8680b1103403c56f32d96
BiswasAngkan <48616181+BiswasAngkan@users.noreply.github.com>
2019-05-01 22:22:38 +0600

    Update LoadImg.py

--- LoadImg.py
+++ LoadImg.py
@@ -5,7 +5,7 @@
 import cv2
 
 colorImg1 = cv2.imread('apple.jpg',1)		#	load an image
-colorImg2=cv2.resize(colorImg1,(256,112))	#	resize the loded image
+colorImg2=cv2.resize(colorImg1,(256,112))	#	resize the loded image when it is very large
 title = 'apple'					#	name of display window
 cv2.imshow(title, colorImg2)			#	display the resize image
 cv2.waitKey(0)					#	hold display window
