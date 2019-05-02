16df64f81b03a353aec45dab77f66704c3a14e5a
BiswasAngkan <48616181+BiswasAngkan@users.noreply.github.com>
2019-05-02 07:48:34 +0600

    Create DisplayImg_SideBySide_1.py

---
+++ DisplayImg_SideBySide_1.py
@@ -0,0 +1,15 @@
+#	Angkan Biswas
+#	02.05.2019
+#	Image show side by side
+
+
+import cv2
+import numpy
+
+loadimg = cv2.imread('/home/dell/Example/apple.jpg',1) 		    # Load image from source folder
+positiveimg = cv2.resize(loadimg, (300,200))				          # Resize image
+negativeImg = 255 - positiveimg					                      # Make negative image
+img = numpy.concatenate((positiveimg,negativeImg), axis=1)		# Add tow image
+title = 'apple'							                                  # Add title
+cv2.imshow(title,img)						                              # Display image
+cv2.waitKey(0)                                                # Image show until any keys press
