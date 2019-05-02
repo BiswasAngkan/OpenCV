26e5056d4696364e8d14e40f8dd6e0670a4d4d1b
BiswasAngkan <48616181+BiswasAngkan@users.noreply.github.com>
2019-05-02 16:58:40 +0600

    Update DisplayImages_Hrizontally.py

--- DisplayImages_Hrizontally.py
+++ DisplayImages_Hrizontally.py
@@ -6,10 +6,10 @@
 import cv2
 import numpy
 
-loadimg = cv2.imread('/home/dell/Example/apple.jpg',1) 		    # Load image from source folder
-positiveimg = cv2.resize(loadimg, (300,200))				          # Resize image
-negativeImg = 255 - positiveimg					                      # Make negative image
-img = numpy.concatenate((positiveimg,negativeImg), axis=1)		# Add tow image
-title = 'apple'							                                  # Add title
-cv2.imshow(title,img)						                              # Display image
-cv2.waitKey(0)                                                # Show images until a key pressed
+loadimg = cv2.imread('/home/dell/Example/apple.jpg',1) 		            # Load image from source folder
+positiveimg = cv2.resize(loadimg, (300,200))				                  # Resize image
+negativeImg = 255 - positiveimg					                              # Make negative image
+img = numpy.concatenate((positiveimg,negativeImg), axis=1)		        # Add tow image
+title = 'apple'							                                          # Add title
+cv2.imshow(title,img)						                                      # Display image
+cv2.waitKey(0)                                                        # Show images until a key pressed
