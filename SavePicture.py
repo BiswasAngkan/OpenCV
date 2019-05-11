35da567f7fdd8cf3de7e494bca218a53d4d2795b
BiswasAngkan <48616181+BiswasAngkan@users.noreply.github.com>
2019-05-11 21:23:50 +0600

    Add files via upload

---
+++ SavePicture.py
@@ -0,0 +1,12 @@
+#	Angkan Biswas
+#	08.05.2019
+#	To save a picture taken by the web camera.
+
+import cv2
+
+camera = cv2.VideoCapture(0)						#	Get access of the web camera.
+cameraStatus, picture = camera.read()					#	Take a picture using the camera.
+imgFileName = '/home/dell/Example/GitHub/Picture/MyPicture.jpg'
+cv2.imwrite(imgFileName, picture)					#	Save picture
+		
+camera.release()							#	Release the captured camera
