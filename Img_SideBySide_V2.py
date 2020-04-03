7dbff1e7a9b8887ebc9ca7d22afa778aa217b829
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-04-03 12:33:05 +0600

    Add files via upload

--- Img_SideBySide_V2.py
+++ Img_SideBySide_V2.py
@@ -4,7 +4,8 @@
 
 
 import cv2
-import numpy
+import numpy as np
+ 
 
 loadImg1 = cv2.imread('/home/dell/Example/apple.jpg',1)			# Load image from source folder
 loadImg2 = cv2.imread('/home/dell/Example/bench.jpg',1)
@@ -15,14 +16,14 @@ positiveImg2 = cv2.resize(loadImg2,(100,100))
 negativeImg1 = 255 - positiveImg1					# Make negative image
 negativeImg2 = 255 - positiveImg2
 
-concateImg1 = numpy.concatenate((positiveImg1,positiveImg2),axis=1)	# Concatenate two images horizontally	
-concateImg2 = numpy.concatenate((negativeImg1,negativeImg2), axis=1)	
-concateImg3 = numpy.concatenate((concateImg1,concateImg2), axis=0)	# Concatenate two images vertically
-concateImg4 = numpy.concatenate((concateImg3,concateImg3), axis=1)	# Concatenate two images after adding two images
+concateImg1 = np.concatenate((positiveImg1,positiveImg2),axis=1)	# Concatenate two images horizontally	
+concateImg2 = np.concatenate((negativeImg1,negativeImg2), axis=1)	
+concateImg3 = np.concatenate((concateImg1,concateImg2), axis=0)		# Concatenate two images vertically
+concateImg4 = np.concatenate((concateImg3,concateImg3), axis=1)		# Concatenate two images after adding two images
 
 print('size of concateImg4: {}'.format(concateImg4.shape))				 
 
-title = 'Fruits~Bench'							# Add title
+title = 'Fruits~Bench'							# Set title
 cv2.imshow(title,concateImg4)						# Display image
 cv2.waitKey(0)								# Show images until a key pressed
 
