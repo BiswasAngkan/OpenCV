4aa9ddd27994445ad3342819688cdb36aa8d53aa
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-04-10 14:33:41 +0600

    Update MakeVideoFile_Positive_Negative.py

--- MakeVideoFile_Positive_Negative.py
+++ MakeVideoFile_Positive_Negative.py
@@ -14,7 +14,7 @@ for i in range(100):
 	negativeFrame = 255 - resizeFrame					#	Negative frame
 	concateFrame = np.concatenate((resizeFrame, negativeFrame), axis = 1) 	#	Add two frames side by side
 	
-	cv2.imshow('RoseBlooming',concateFrame)					#	Display the frame.
+	cv2.imshow('My Face',concateFrame)					#	Display the frame.
 	cv2.waitKey(10)								#	Hold the display window for 50 millisecond
 
 camera.release()								#	Release the captured camera
