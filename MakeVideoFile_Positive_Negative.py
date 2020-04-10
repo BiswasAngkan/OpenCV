bb09e3e6b4bf300e2b68beed93e87a4e0c31ac6b
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-04-10 14:26:42 +0600

    Add files via upload

---
+++ MakeVideoFile_Positive_Negative.py
@@ -0,0 +1,30 @@
+#	Angkan Biswas
+#	08.05.2019
+#	Capture a video file.
+
+import cv2
+import numpy as np
+
+camera = cv2.VideoCapture(0)							#	Open a cemera.
+
+for i in range(100): 
+	_, frame = camera.read()						#	Take a frame.
+	
+	resizeFrame = cv2.resize(frame,(300,200))				# 	Resize the freame.
+	negativeFrame = 255 - resizeFrame					#	Negative frame
+	concateFrame = np.concatenate((resizeFrame, negativeFrame), axis = 1) 	#	Add two frames side by side
+	
+	cv2.imshow('RoseBlooming',concateFrame)					#	Display the frame.
+	cv2.waitKey(10)								#	Hold the display window for 50 millisecond
+
+camera.release()								#	Release the captured camera
+
+
+
+
+
+
+
+
+
+
