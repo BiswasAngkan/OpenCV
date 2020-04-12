659540e1b91062e75c53227333d7974a628995d1
Biswas Angkan <48616181+BiswasAngkan@users.noreply.github.com>
2020-04-12 22:42:40 +0600

    Add files via upload

---
+++ Rotate_Image.py
@@ -0,0 +1,22 @@
+# Angkan Bsiwas
+# 12.04.2020
+# To rotate an image
+
+
+import cv2
+import imutils
+import numpy as np
+
+
+imgPath = '/home/dell/PythonCode/Picture/apple.jpg'		
+img = cv2.imread(imgPath)					# to load an image
+
+resizeImg = cv2.resize(img, (200,200))				# to resize the loaded image
+
+degree = 10
+rotateImg = imutils.rotate(resizeImg, degree)			# to rotate the resize image
+
+concateImg = np.concatenate((resizeImg, rotateImg), axis = 1)	# to concatenate two images	
+
+cv2.imshow('Picture', concateImg)				# to display concatenated images
+cv2.waitKey(0)							# to hold the display window untill any key is pressed
